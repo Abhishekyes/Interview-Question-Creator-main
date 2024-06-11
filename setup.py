fb0de808f306e6495e22971497096cde5d08f7b0
Abhishekyes <vvabhi2776@gmail.com>
2024-06-11 18:37:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Gen AI Project",
+    version= '0.0.0',
+    author="Ahbishek",
+    author_email="vvabhi2776@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
